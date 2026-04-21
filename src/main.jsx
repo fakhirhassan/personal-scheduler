4012ea4a81528b437101ee350515e1ab5c5bbc6e
Fakhir Hassan <134173573+fakhirhassan@users.noreply.github.com>
2026-04-21 22:13:53 +0500

    ready for deployment

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Planner from '../planner.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Planner />
+  </React.StrictMode>
+);
